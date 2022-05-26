88eb92cc29a13300dab4ae49a846ad9d5b8d28a7
Joao Delfino <joaohsdelfino@gmail.com>
2022-05-25 22:33:31 -0300

    Api project configured

---
+++ api-app-challenge/src/test/java/com/api/apiappchallenge/ApiAppChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apiappchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAppChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
