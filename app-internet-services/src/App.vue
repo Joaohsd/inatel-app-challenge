445cbd552ffad3cce75407a705d97ec68c469a53
Joao Delfino <joaohsdelfino@gmail.com>
2022-05-27 00:00:22 -0300

    Createdheader component for navigation bar

---
+++ app-internet-services/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <HeaderHome />
+    <v-main>
+      <router-view/>
+    </v-main>
+  
+  </v-app>
+</template>
+
+<script>
+import HeaderHome from '@/components/Header-home.vue'
+export default {
+    name: "App",
+    components: { HeaderHome },
+    data: () => ({
+      //
+    }),
+};
+</script>
---
+++ app-internet-services/src/components/Header-home.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer 
+      v-model="drawer"
+      app
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Provider App
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            The best way to find a good internet service.
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+        right: null,
+      }
+    },
+  }
+</script>
