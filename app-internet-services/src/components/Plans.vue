7069c2af468f49a122a3e553131cdc7fb20012e6
Joao Delfino <joaohsdelfino@gmail.com>
2022-05-29 00:00:03 -0300

    Plan view working right

--- app-internet-services/src/components/Plans.vue
+++ app-internet-services/src/components/Plans.vue
@@ -2,72 +2,74 @@
   <div class="plans">
     <HeaderCliente></HeaderCliente>
     <div class="planos">
-      <div class="select">
-        <div class="select-state">
-          <v-row align="center">
-            <v-col cols="5">
-              <v-subheader>
-                Selecione o estado
-              </v-subheader>
-            </v-col>
-            <v-col cols="6">
-              <v-select
-                v-model="select"
-                :hint="`${select.state}, ${select.abbr}`"
-                :items="states"
-                item-text="state"
-                item-value="abbr"
-                label="Select"
-                persistent-hint
-                return-object
-                single-line
-                v-on:change="getPlansFromState"
-              ></v-select>
-            </v-col>
-          </v-row>
-        </div>
-      </div>
-      <div class="informacoes">
-          <div class="carrossel">
-            <div class="carrossel-int">
-              <v-carousel hide-delimiters cycle>
-                <v-carousel-item
-                    v-for="(plano,i) in planos"
-                    :key="i"
-                >
-                  <h2>Plano {{i+1}}</h2>
-                  <v-row>
-                    <h3 id="key-plan">Provedor:</h3>
-                    <h3>{{plano.isp}}</h3>
-                  </v-row>
-                  <v-row>
-                    <h3 id="key-plan">Capacidade de Dados:</h3>
-                    <h3>{{plano.data_capacity}}</h3>
-                  </v-row>
-                  <v-row>
-                    <h3 id="key-plan">Taxa de Download:</h3>
-                    <h3>{{plano.download_speed}}</h3>
-                  </v-row>
-                  <v-row>
-                    <h3 id="key-plan">Taxa de Upload:</h3>
-                    <h3>{{plano.upload_speed}}</h3>
-                  </v-row>
-                  <v-row>
-                    <h3 id="key-plan">Descrição:</h3>
-                    <h3>{{plano.description}}</h3>
-                  </v-row>
-                  <v-row>
-                    <h3 id="key-plan">Preço mensal:</h3>
-                    <h3>R${{plano.price_per_month}}/mês</h3>
-                  </v-row>
-                  <v-row>
-                    <h3 id="key-plan">Tipo de internet:</h3>
-                    <h3>{{plano.type_of_internet}}</h3>
-                  </v-row>
-                </v-carousel-item>
-              </v-carousel>
-            </div>
+      <div class="main-content">
+        <div class="content">
+        <div class="select">
+          <div class="select-state">
+            <v-row align="center">
+              <v-col cols="5">
+                <v-subheader>
+                  Selecione o estado
+                </v-subheader>
+              </v-col>
+              <v-col cols="6">
+                <v-select
+                  v-model="select"
+                  :items="states"
+                  item-text="state"
+                  item-value="abbr"
+                  label="Estado"
+                  return-object
+                  single-line
+                  v-on:change="getPlansFromState"
+                ></v-select>
+              </v-col>
+            </v-row>
           </div>
+        </div>
+        <div class="informacoes">
+            <div class="carrossel">
+              <div class="carrossel-int">
+                <v-carousel hide-delimiters cycle>
+                  <v-carousel-item
+                      v-for="(plano,i) in planos"
+                      :key="i"
+                  >
+                    <h2>Plano {{i+1}}</h2>
+                    <v-row>
+                      <h3 id="key-plan">Provedor:</h3>
+                      <h3>{{plano.isp}}</h3>
+                    </v-row>
+                    <v-row>
+                      <h3 id="key-plan">Capacidade de Dados:</h3>
+                      <h3>{{plano.data_capacity}}</h3>
+                    </v-row>
+                    <v-row>
+                      <h3 id="key-plan">Taxa de Download:</h3>
+                      <h3>{{plano.download_speed}}</h3>
+                    </v-row>
+                    <v-row>
+                      <h3 id="key-plan">Taxa de Upload:</h3>
+                      <h3>{{plano.upload_speed}}</h3>
+                    </v-row>
+                    <v-row>
+                      <h3 id="key-plan">Descrição:</h3>
+                      <h3>{{plano.description}}</h3>
+                    </v-row>
+                    <v-row>
+                      <h3 id="key-plan">Preço mensal:</h3>
+                      <h3>R${{plano.price_per_month}}/mês</h3>
+                    </v-row>
+                    <v-row>
+                      <h3 id="key-plan">Tipo de internet:</h3>
+                      <h3>{{plano.type_of_internet}}</h3>
+                    </v-row>
+                  </v-carousel-item>
+                </v-carousel>
+              </div>
+            </div>
+        </div>
+        </div>
       </div>
     </div>
   </div>
@@ -127,6 +129,8 @@
     flex-direction: row;
     justify-content: center;
     align-items: center;
+    background-color: rgba(240, 248, 255, 0.87);
+    border-radius: 12px 12px 1px 1px;
   }
 
   .informacoes{
@@ -137,6 +141,24 @@
     align-items: center;
   }
 
+  .main-content{
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .main-content .content{
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(12, 11, 11, 0.884);
+    border-radius: 12px;
+  }
+
   .carrossel .carrossel-int{
     display: flex;
     flex-direction: column;
@@ -145,8 +167,6 @@
     padding: 25px;
     width: 430px;
     height: 520px;
-    border-radius: 12px;
-    background-color: rgba(12, 11, 11, 0.884);
   }
 
   .carrossel .carrossel-int #key-plan{
